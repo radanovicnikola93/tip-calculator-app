c4844ca2bebaa97183a8bc16e1080cd5fd923ab8
Nikola Radanovic <radanovicnikola93@gmail.com>
2022-03-02 17:39:51 +0100

    updated functions

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ const resetBtn = document.getElementById("reset-all-btn");
 const btnarray = [...tipBtnAll];
 
 // grab people input number value
-const getBillValue = billInput.onkeyup = function() {
-    console.log(billInput.value)
+billInput.onkeyup = function() {
+    return billInput.value
 }
 
 // grab people input number value
-const getPeopleValue = peopleInput.onkeyup = function () {
-    console.log(peopleInput.value);
+peopleInput.onkeyup = function() {
+    return peopleInput.value
 };
 
 // grab btn value
-const getBtnValue = btnarray.map(btn => {
+btnarray.map(btn => {
     btn.addEventListener('click', (e) => {
-        console.log(e.target.value)
+        return e.target.value
     })
 })
